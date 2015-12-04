c68b0ef4598d84cbcdba6039692b556dba94e388
Mark Goody <mgoody@shopkeep.com>
2015-12-04 17:51:47 -0500

    Present the text input controller

--- Textatic/Textatic WatchKit Extension/InterfaceController.swift
+++ Textatic/Textatic WatchKit Extension/InterfaceController.swift
@@ -8,7 +8,22 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var label: WKInterfaceLabel!
     
     @IBAction func buttonTapped() {
+        let suggestions = [
+            "Game over, man! Game over!",
+            "Hello. My name is Inigo Montoya.",
+            "That's a big Twinkie.",
+            "Let's see what happens when we take away the puppy.",
+            "In the end there can be only one."
+        ]
         
+        presentTextInputControllerWithSuggestions(suggestions, allowedInputMode: .Plain) { responses in
+            if let responses = responses where responses.count > 0 {
+                if let response = responses.first as? String {
+                    self.text = response
+                    self.label.setText(self.text)
+                }
+            }
+        }
     }
     
     private var text = "Tap the button to say something"
@@ -18,5 +33,6 @@ class InterfaceController: WKInterfaceController {
         
         label.setText(text)
     }
+    
 
 }
