6d5b7a6a485d6f7f5531f47e08dc71704be8d655
Mark Goody <mgoody@shopkeep.com>
2015-12-04 17:54:02 -0500

    Handle an emoji input

--- Textatic/Textatic WatchKit Extension/InterfaceController.swift
+++ Textatic/Textatic WatchKit Extension/InterfaceController.swift
@@ -16,17 +16,21 @@ class InterfaceController: WKInterfaceController {
             "In the end there can be only one."
         ]
         
-        presentTextInputControllerWithSuggestions(suggestions, allowedInputMode: .Plain) { responses in
+        presentTextInputControllerWithSuggestions(suggestions, allowedInputMode: .AllowAnimatedEmoji) { responses in
             if let responses = responses where responses.count > 0 {
                 if let response = responses.first as? String {
                     self.text = response
                     self.label.setText(self.text)
+                } else if let response = responses.first as? NSData {
+                    self.imageData = response
+                    self.image.setImageData(self.imageData)
                 }
             }
         }
     }
     
     private var text = "Tap the button to say something"
+    private var imageData: NSData?
     
     override func willActivate() {
         super.willActivate()
